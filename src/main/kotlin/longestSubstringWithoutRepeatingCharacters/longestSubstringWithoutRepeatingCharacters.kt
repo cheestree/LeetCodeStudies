ad7134f1f36b0fc4ff9424ebdab2b817f15ea38d
DQTree <118579040+DQTree@users.noreply.github.com>
2023-11-28 00:41:25 +0000

    Added findMedianSortedArrays, longestSubstringWithoutRepeating, twoSum, mergeTwoSortedLists

---
+++ src/main/kotlin/longestSubstringWithoutRepeatingCharacters/longestSubstringWithoutRepeatingCharacters.kt
@@ -0,0 +1,17 @@
+package longestSubstringWithoutRepeatingCharacters
+
+fun lengthOfLongestSubstring(s: String): Int {
+    var temp = 0
+
+    for (i in s.indices) {
+        var j = i + 1
+        while (j < s.length && s[j] !in s.substring(i, j)) {
+            j++
+        }
+
+        val substring = s.substring(i, j)
+        temp = maxOf(temp, substring.length)
+    }
+
+    return temp
+}
---
+++ src/test/kotlin/findMedianSortedArrays/FindMedianSortedArraysTest.kt
@@ -0,0 +1,24 @@
+package findMedianSortedArrays
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class FindMedianSortedArraysTest {
+    @Test
+    fun findMedianSortedArrays1(){
+        val arr1 = intArrayOf(1, 3)
+        val arr2 = intArrayOf(2)
+        val output = findMedianSortedArrays(arr1, arr2)
+        val correctOutput = 2.0
+        assertEquals(correctOutput, output)
+    }
+
+    @Test
+    fun findMedianSortedArrays2(){
+        val arr1 = intArrayOf(1, 2)
+        val arr2 = intArrayOf(3,4)
+        val output = findMedianSortedArrays(arr1, arr2)
+        val correctOutput = 2.5
+        assertEquals(correctOutput, output)
+    }
+}
